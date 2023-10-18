4864cb3c4422340f6d565dca37c861e898151583
lisatordai <lisatordai@gmail.com>
2023-10-18 12:10:25 -0500

    dashboard

---
+++ Midterm/dashboard.js
@@ -0,0 +1,48 @@
+(() => {
+    'use strict'
+  
+    // Graphs
+    const ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday'
+        ],
+        datasets: [{
+          data: [
+            50,
+            60,
+            50,
+            69,
+            72,
+            70,
+            75
+          ],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            boxPadding: 3
+          }
+        }
+      }
+    })
+  })()
+  
