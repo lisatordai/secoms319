4d0a8469390db6034032b3c567e910be22b3a87c
lisatordai <lisatordai@gmail.com>
2023-10-19 00:34:56 -0500

    index done

--- Midterm/index.js
+++ Midterm/index.js
@@ -2,27 +2,34 @@
 // ISU Netid : ltordai@iastate.edu
 // Date : October 04th, 2023
 fetch("./data_index.json")
-.then(response => response.json())
-.then(myResearchTopics => loadMovies(myMovies));
+  .then(response => response.json())
+  .then(myTopics => loadTopics(myTopics));
 
-function loadMovies(myMovies){
-    var mainContanier = document.getElementById("goodmovies");
-    console.log(myMovies);
-    for (var i=0; i<myMovies.movies.length; i++){
-        let title = myMovies.movies[i].title;
-        let year = myMovies.movies[i].year;
-        let url = myMovies.movies[i].url;
+function loadTopics(myTopics) {
+  var mainContainer = document.getElementById("indextopics");
+  //console.log(myTopics);
+  // Create a new row for each set of columns
+  let row = document.createElement("div");
+  row.className = "row";
+  for (var i = 0; i < myTopics.researchTopics.length; i++) {
+    let title = myTopics.researchTopics[i].title;
+    let text = myTopics.researchTopics[i].text;
+    let url = myTopics.researchTopics[i].url;
 
-        console.log(title);
+    //console.log(title);
 
-        //DOM
-        let div = document.createElement("div");
-        div.innerHTML = `
-         <h3>${title}</h3> <br>
-        ${year} <br>
-        <img src=${url} width="200"> <br> <br> 
-        `;
+    //DOM
+    // Create a column for the element
+    let column = document.createElement("div");
+    column.className = "col-lg-4";
+    column.innerHTML = `
+      <h2 class="fw-normal"><strong>${title}</strong></h2>
+      <p>${text}</p>
+      <p><a class="btn btn-secondary" href="${url}">View details &raquo;</a></p>
+    `;
 
-        mainContanier.appendChild(div);
-    }
+    // Append the column to the row and the row to the main container
+    row.appendChild(column);
+    mainContainer.appendChild(row);
+  }
 }
