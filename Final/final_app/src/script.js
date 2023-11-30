eb97afb662572182da658b23b3a305d0598af9af
jayaed16 <jayaed16@iastate.edu>
2023-11-29 19:55:02 -0600

    3 pages donr projects now uses json

--- Final/final_app/src/script.js
+++ Final/final_app/src/script.js
@@ -13,7 +13,7 @@ const Header = (props) => {
   return (
     <div>
       <nav class="navbar navbar-dark">
-        <div style = {{marginLeft:"135px", marginRight:"135px"}} class="container-fluid">
+        <div style = {{marginLeft:"155px", marginRight:"155px"}} class="container-fluid">
           <div class="row w-100 justify-content-between">
             <div class="col-auto text-left">
               <a href="https://www.iastate.edu" class="btn btn-outline-primary">iastate.edu</a>
@@ -29,16 +29,16 @@ const Header = (props) => {
         </div>
       </nav>
       
-      <div className="red-header left-aligned">
-      <img  style={{marginLeft:"135px"}}  src="https://greenhouse.eeob.iastate.edu/themes/isubit/iastate8_theme/logo.svg" alt="Logo" />
-        <p  style={{marginLeft:"1555px"}} ><strong>Shared Plant Growth Facilities</strong></p>
+      <div className="red-header left-aligned container-fluid">
+      <img  style={{marginLeft:"180px"}}  src="https://greenhouse.eeob.iastate.edu/themes/isubit/iastate8_theme/logo.svg" alt="Logo" />
+        <p  style={{marginLeft:"180px", marginTop:"5px"}} ><strong>Shared Plant Growth Facilities</strong></p>
       </div>
 
-      <nav class="navbar2 navbar-dark">
+      <nav class="navbar2 navbar-dark" >
         <div class="container-fluid">
-          <div style={{marginLeft:"135px"}} class="row w-100 justify-content-between">
+          <div style={{marginLeft:"155px"}} class="row w-100 justify-content-between">
             <div class="col-auto text-left">
-              <a class="btn btn-outline-primary"  onClick={() => { props.setView('page1') }}>page1</a>
+              <a class="btn btn-outline-primary"  onClick={() => { props.setView('page1') }}>Home</a>
               <a  class="btn btn-outline-primary"  onClick={() => { props.setView('page2') }}>page2</a>
               <a class="btn btn-outline-primary" onClick={() => { props.setView('page3') }}>page3</a>
               <a class="btn btn-outline-primary" onClick={() => { props.setView('the Greenhouses') }}>Greenhouses</a>
@@ -102,6 +102,30 @@ const Content = (props) => {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from Projects.json
+    fetch('./Projects.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Fetched data:', data);
+        setProjects(data);
+        console.log('test 1', projects);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  let i = projects.length;
   
 
   let content;
@@ -149,10 +173,10 @@ const Content = (props) => {
 
       case 'the Greenhouses':
       content = ( 
-        <div>
-          <h1 style={{marginTop:"10px"}}>About the Greenhouses</h1>
-        <div className="padding">
-          <div id="plant-container" className="row row-cols-1 row-cols-md-3 g-4">
+        <div id="green-house-container">
+          <h1 style={{marginTop:"30px"}}>About the Greenhouses</h1>
+        
+          <div  className="row row-cols-1 row-cols-md-3 g-4">
             {GreenHouse.House.map((GreenHouse, index) => (
               <div key={index} className="col">
                 <div className="card h-100">
@@ -164,7 +188,7 @@ const Content = (props) => {
                 </div>
               </div>
             ))}
-          </div>
+          
         </div>
       
       
@@ -174,82 +198,25 @@ const Content = (props) => {
 
       case 'page5':
       content = ( 
-      <div>
-          
-          <h1 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Current Research</h1>
-          <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-          Welcome to the Shared Plant Growth Facilities Current Research page.
-          Our facilities support a range of projects across various disciplines,
-          providing a platform for scientific curiosity and discovery.Here are a
-          few activities within our shared spaces that we are currently working on. 
-          </p>
-
-          <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px", color: "#CC0001" }} />
-
-          <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Agronomy Greenhouse room 131</h4>
-          <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            With the evolution of greenhouse lighting technology there
-            comes obsolescence. Current lighting technology is rumored 
-            to be on its way to obsolescence in the near future. Most LED
-            lighting research has been conducted with plants that are 
-            much shorter than corn and soybeans at full maturity so a
-            lighting trial is being conducted to see if those crops can 
-            be successful under standard toplighting or if additional
-            methods need to be investigated before the current lighting 
-            technology is gone. 
-            </p>
-
-          <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-          <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Horticulture Hall rooms 82 and 86</h4>
-          <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-          The ISU Horticulture Club is producing a Poinsettia crop to be sold 
-          before the end of the semester.  
-            </p>
-
-            <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-            <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Agronomy Hall Room 116 and ATRB room 6302</h4>
-            <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            The Iowa Monarch Conservation Consortium is raising milkweed plants to feed Monarch
-             butterfly butterfly caterpillars in their colony.   
-              </p>
-
-              <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-            <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Plant Pathology Greenhouse room 115</h4>
-            <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            An experiment on soil applied herbicides and their effect on weed seed germination.  
-              </p>
-
-              <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-            <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Plant Pathology Greenhouse room 121</h4>
-            <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            A winter seed increase on soybeans for use in field trials for summer 2024.
-             </p>
-
-             <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-            <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Agronomy Greenhouse room 119</h4>
-            <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            Genetic research on Sorghum plants for food and biofuel uses.
-            </p>
-
-            <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-            <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Horticulture Hall room 46J</h4>
-            <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            Research on Mungbean to investigate its suitability as a staple crop in North America. 
-            </p>
-
-            <hr style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }} />
-
-            <h4 style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>Agronomy Greenhouse and Agronomy Hall – multiple chambers</h4>
-            <p style={{marginTop:"10px", marginLeft: "100px" , marginRight: "100px"}}>
-            Climate change research investigating soybean production under different climate parameters. 
-            </p>
+       <div id="green-house-container">
+        <h1>Current Research</h1>
+                <p>Welcome to the Shared Plant Growth Facilities Current Research page. Our facilities support a range of projects across various disciplines, providing a platform for scientific curiosity and discovery. Here are a few activities within our shared spaces that we are currently working on.</p>
+                <hr style={{ marginTop: "20px", color: "#CC0001"}} />
+    
+        
+          {projects.Projects.length > 0 ? (
+            projects.Projects.map((item, index) => (
+              <div key={index}>
+                <h3>{item.header}</h3>
+                <p>{item.info}</p>
+                <hr style={{ marginTop: "20px"}} />
+              </div>
+            ))
+          ) : (
+            <p>Loading... {i}</p>
+          )}
         </div>
+  
       );
       break;
 
