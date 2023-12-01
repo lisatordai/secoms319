683bb087b9492334f6d087adcf8d841e759dbba4
lisatordai <lisatordai@gmail.com>
2023-12-01 10:04:36 -0600

    backend working with sql

--- Final/backend/appbackend.js
+++ Final/backend/appbackend.js
@@ -5,6 +5,33 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
+
+
+
+// Route to get all posts
+app.get("/api/get", (req, res) => {
+    db.query("SELECT * FROM fakestore_catalog", (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Route to get one post
+app.get("/api/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    db.query(
+        "SELECT * FROM fakestore_catalog WHERE id = ?", id,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            res.send(result);
+        }
+    );
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
