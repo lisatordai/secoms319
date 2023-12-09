a461e780e428327cbc8b9a0dac113ded11f6d35d
lisatordai <lisatordai@gmail.com>
2023-12-09 01:01:34 -0600

    methods

--- Final/backend/appbackend.js
+++ Final/backend/appbackend.js
@@ -38,7 +38,7 @@ client.connect(err => {
     }
 });
 
-// HOME/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// HOME
 // Get all posts
 app.get("/api/home/get", (req, res) => {
     const collection = database.collection("home");
@@ -74,6 +74,198 @@ app.get("/api/home/getFromTitle/:title", (req, res) => {
     });
 });
 
+
+// MANAGER
+// Get all manager posts
+app.get("/api/manager/get", (req, res) => {
+    const collection = database.collection("manager");
+    collection.find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get manager from Id
+app.get("/api/manager/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    const collection = database.collection("manager");
+    collection.findOne({ id: parseInt(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get manager from name
+app.get("/api/manager/getFromName/:name", (req, res) => {
+    const name = req.params.name;
+    const collection = database.collection("manager");
+    collection.findOne({ name: name }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// GREENHOUSE
+// Get all greenhouse posts
+app.get("/api/greenhouse/get", (req, res) => {
+    const collection = database.collection("greenhouse");
+    collection.find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get greenhouse from Id
+app.get("/api/greenhouse/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    const collection = database.collection("greenhouse");
+    collection.findOne({ id: parseInt(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get greenhouse from name
+app.get("/api/greenhouse/getFromName/:name", (req, res) => {
+    const name = req.params.name;
+    const collection = database.collection("greenhouse");
+    collection.findOne({ name: name }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// CURRENT RESEARCH
+// Get all current_research posts
+app.get("/api/research/get", (req, res) => {
+    const collection = database.collection("current_research");
+    collection.find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get current_research from Id
+app.get("/api/research/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    const collection = database.collection("current_research");
+    collection.findOne({ id: parseInt(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get current_research from title
+app.get("/api/research/getFromTitle/:title", (req, res) => {
+    const title = req.params.title;
+    const collection = database.collection("current_research");
+    collection.findOne({ title: title }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// GREENHOUSE SPACE RATES
+// Get all space posts
+app.get("/api/space/get", (req, res) => {
+    const collection = database.collection("greenhouse_space_rates");
+    collection.find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get space from Id
+app.get("/api/space/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    const collection = database.collection("greenhouse_space_rates");
+    collection.findOne({ id: parseInt(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// GREENHOUSE CHAMBER RENTAL RATES
+// Get all chamber rental posts
+app.get("/api/chamber/get", (req, res) => {
+    const collection = database.collection("growth_chamber_rental_rates");
+    collection.find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get chamber rental from Id
+app.get("/api/chamber/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    const collection = database.collection("growth_chamber_rental_rates");
+    collection.findOne({ id: parseInt(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get chamber rental from period (daily weekly)
+app.get("/api/chamber/getFromPeriod/:rental_period", (req, res) => {
+    const rental_period = req.params.rental_period;
+    const collection = database.collection("growth_chamber_rental_rates");
+    collection.find({ rental_period: rental_period }).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// MANAGER RESPONSIBILITIES
+// Get all responsibilities section posts
+app.get("/api/responsibilities/get", (req, res) => {
+    const collection = database.collection("manager_responsibilities");
+    collection.find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
+
+// Get responsibilities section from Id
+app.get("/api/responsibilities/getFromId/:id", (req, res) => {
+    const id = req.params.id;
+    const collection = database.collection("manager_responsibilities");
+    collection.findOne({ id: parseInt(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
