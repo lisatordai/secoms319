2a8424b1f238faf4b19254751339ed296f4a8eb7
lisatordai <lisatordai@gmail.com>
2023-12-13 09:03:19 -0600

    small change

--- Final/frontend/final_app/src/script.js
+++ Final/frontend/final_app/src/script.js
@@ -306,7 +306,7 @@ const Content = (props) => {
         var container = document.getElementById("showData");
         container.innerHTML = JSON.stringify(data);
       })
-      .catch((err) => console.log("Notice:" + err));
+      .catch((err) => console.log("Notice: " + err));
       // .catch((err) => console.log("Error:" + err));
   }
 
